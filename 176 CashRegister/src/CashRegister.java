3db58f90f33903bdf56af38d0d63488d5ca66183
Isabella Chiaravalloti <isabellachiarav@gmail.com>
2020-01-29 20:38:32 -0500

    final commit

---
+++ 176 CashRegister/src/CashRegister.java
@@ -0,0 +1,30 @@
+
+public class CashRegister {
+
+	private double payment;
+	private double purchase;
+	double change;
+	
+	//constructor to create a cash register with purchase = 0 and payment = 0
+	public CashRegister() {
+		purchase = 0;
+		payment = 0;
+	}
+	
+	//recordPurchase will record a sale and add the new sale amount to purchase
+	public void recordPurchase(double sale) {
+		purchase += sale;
+	}
+	
+	//receivePayment will receive the amount paid by the customer into payment
+	public void receivePayment(double paymentAmount) {
+		payment += paymentAmount;
+	}
+	
+	//giveChange will calculate and return the amount of change due to the customer (purchase - payment)
+	public double giveChange() {
+		return change = payment - purchase;
+	}
+	
+	
+}
---
+++ 176 CashRegister/src/CashRegisterTester.java
@@ -0,0 +1,35 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class CashRegisterTester {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		double sale = 0, paymentAmount = 0;
+		final int SENTINEL = -1;
+		boolean purchaseLoop = true;
+		NumberFormat fmt = NumberFormat.getCurrencyInstance();
+		
+		//create a CashRegister object
+		CashRegister register1 = new CashRegister();
+		
+		
+		//use a loop to accept purchases from the screen until a SENTINEL value of -1 is typed
+		while (sale != -1) {
+			register1.recordPurchase(sale);
+			System.out.print("Enter a purchase amount (type '-1' to stop): ");
+			sale = scan.nextDouble();
+		}
+		
+		//accept payment for the purchases
+		System.out.println("Enter payment: ");
+		paymentAmount = scan.nextDouble();
+		register1.receivePayment(paymentAmount);
+		
+		//calculate and print the amount of change due to the customer
+		System.out.println("Your change: " + fmt.format(register1.giveChange()));
+
+	}
+
+}
